f670ff52bbc0d1b7d2cabc4eda5ca74d5e7d864d
SanjayS808 <sanjaysugumar20@gmail.com>
2024-07-24 22:26:24 -0500

    changes

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./card.css";
 import Image from "next/image";
 import p1 from "/public/p1.jpeg";
-import p2 from "/public/p2.jpg";
+import p2 from "/public/p2.png";
 
 export default function Projects() {
     return (
@@ -31,7 +31,7 @@ export default function Projects() {
         <div className="flex justify-center  p-10 font-black text-4xl">
           <h1>Here are some of my projects!</h1>
           </div>        
-        <div className="flex justify-center px-10 ">
+        <div id= "card" className="flex justify-center px-10 ">
                     <div class="card">
             <div class="card-inner">
                 <div class="card-front">
@@ -44,6 +44,30 @@ export default function Projects() {
                       <li className="py text-sm font-bold "> React, Java, Python, SQL</li>
                       <li className=" text-sm p-2"><p>•A point of sale system for a fictional boba shop capable of taking orders, inventory management, and generating sales analytics.</p></li>
                       <li className=" text-sm p-2"><p>•Uses a PostgreSQL database for backend data storage and a React.js frontend for an intuitive UI.</p></li>
+
+                    </ul>                      
+
+                  </div>
+              </div>
+            </div>
+          
+          
+          
+        </div>
+        <div id= "card" className="flex justify-center px-10 ">
+                    <div class="card">
+            <div class="card-inner">
+                <div class="card-front">
+                <Image src={p2} className="rounded-lg" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                </div>
+                <div class="card-back">
+                    <ul className="p-5">
+
+                      <li className=" text-xl font-bold "> <a href = "https://github.com/SanjayS808/Seam-Carver"><span class="border-b border-solid border-black border-b-3">PPM File Compressor</span></a></li>
+                      <li className="py text-sm font-bold "> C++</li>
+                      <li className=" text-sm p-2"><p>• Implemented a content aware PPM file compressor using the seam carving technique in C++, effectively reducing image size while preserving essential features.</p></li>
+                      <li className=" text-sm p-2"><p>• Demonstrated proficiency in algorithm design and implementation, optimizing the compression process for efficient and seamless file manipulation.</p></li>
+                      
 
                     </ul>                      
 
